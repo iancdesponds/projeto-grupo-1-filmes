46f2632e66dfed2fa02cbe5b76029ef490ad2808
Esther <estherccr@al.insper.edu.br>
2024-10-28 11:12:39 -0300

    criar filme

--- filmes/src/main/java/br/insper/filmes/filme/FilmeService.java
+++ filmes/src/main/java/br/insper/filmes/filme/FilmeService.java
@@ -1,4 +1,31 @@
 package br.insper.filmes.filme;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
 public class FilmeService {
+
+    @Autowired
+    FilmeRepository filmeRepository;
+
+    public Filme CriarFilme(Filme filme) {
+        if (filme == null) {
+            throw new IllegalArgumentException("Filme não pode ser nulo");
+        }
+
+        if (filme.getTitulo() == null || filme.getAtores() == null || filme.getClassificacao() == null || filme.getDescricao() == null || filme.getDiretores() == null || filme.getGenero() == null || filme.getAno() == null) {
+            throw new IllegalArgumentException("Campos obrigatórios estão ausentes");
+        }
+
+        filme.setId(UUID.randomUUID().toString());
+
+        return filmeRepository.save(filme);
+
+
+    }
 }
