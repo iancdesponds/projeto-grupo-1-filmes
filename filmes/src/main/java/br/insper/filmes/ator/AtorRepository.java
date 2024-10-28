fbffd9849915b405a27fe1084672e0e280bd7e77
ribollequis87 <gustavocr2@al.insper.edu.br>
2024-10-28 11:46:15 -0300

    Classe Ator Repository

--- filmes/src/main/java/br/insper/filmes/ator/AtorRepository.java
+++ filmes/src/main/java/br/insper/filmes/ator/AtorRepository.java
@@ -1,4 +1,8 @@
 package br.insper.filmes.ator;
 
-public interface AtorRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtorRepository extends MongoRepository<Ator, Integer> {
 }
