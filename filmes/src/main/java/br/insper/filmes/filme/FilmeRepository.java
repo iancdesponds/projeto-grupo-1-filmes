adcd7423169fd7f37a6a77b45d3d88d3c4e97503
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-10-28 11:28:29 -0300

    repository

--- filmes/src/main/java/br/insper/filmes/filme/FilmeRepository.java
+++ filmes/src/main/java/br/insper/filmes/filme/FilmeRepository.java
@@ -2,5 +2,10 @@ package br.insper.filmes.filme;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends MongoRepository<Filme, String> {
+    List<Filme> findByGenero(String genero);
+    List<Filme> findByAno(Integer ano);
+    List<Filme> findByClassificacao(String classificacao);
 }
