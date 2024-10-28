1ddc7268b4280eb14816386e0f72a41d3f7aa723
ribollequis87 <gustavocr2@al.insper.edu.br>
2024-10-28 11:45:29 -0300

    Classe Ator Controller

--- filmes/src/main/java/br/insper/filmes/ator/AtorController.java
+++ filmes/src/main/java/br/insper/filmes/ator/AtorController.java
@@ -1,4 +1,50 @@
 package br.insper.filmes.ator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/atores")
 public class AtorController {
+
+    @Autowired
+    private AtorService atorService;
+
+    @PostMapping
+    public ResponseEntity<Ator> criarAtor(@Validated @RequestBody Ator ator) {
+        Ator novoAtor = atorService.criarAtor(ator);
+        return ResponseEntity.ok(novoAtor);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ator> buscarAtorPorId(@PathVariable("id") Integer id) {
+        Optional<Ator> ator = atorService.buscarAtorPorId(id);
+        return ator.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ator>> listarTodosAtores() {
+        return ResponseEntity.ok(atorService.listarTodosAtores());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ator> atualizarAtor(@PathVariable("id") Integer id, @Validated @RequestBody Ator ator) {
+        Optional<Ator> atorAtualizado = atorService.atualizarAtor(id, ator);
+        return atorAtualizado.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarAtor(@PathVariable("id") Integer id) {
+        if (atorService.deletarAtor(id)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
