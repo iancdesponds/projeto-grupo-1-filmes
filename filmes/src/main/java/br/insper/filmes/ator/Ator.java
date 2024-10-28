fa79b419c8deb2971ec28d3eec42e212177df0c1
ribollequis87 <gustavocr2@al.insper.edu.br>
2024-10-28 11:45:03 -0300

    Classe Ator

--- filmes/src/main/java/br/insper/filmes/ator/Ator.java
+++ filmes/src/main/java/br/insper/filmes/ator/Ator.java
@@ -1,4 +1,60 @@
 package br.insper.filmes.ator;
 
+import java.util.List;
+import br.insper.filmes.filme.Filme;
+
 public class Ator {
+    private Integer id;
+    private String nome;
+    private String nacionalidade;
+    private String biografia;
+    private List<Filme> filmes;
+
+    public Ator(Integer id, String nome, String nacionalidade, String biografia, List<Filme> filmes) {
+        this.id = id;
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.biografia = biografia;
+        this.filmes = filmes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
 }
