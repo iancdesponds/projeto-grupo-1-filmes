e53e7f92b0981039946144011b580feca667e3fb
RafaelaAfférri <rafaelaao@al.insper.edu.br>
2024-10-28 11:12:16 -0300

    filme rota de atualizar e deletar

--- filmes/src/main/java/br/insper/filmes/filme/FilmeService.java
+++ filmes/src/main/java/br/insper/filmes/filme/FilmeService.java
@@ -1,4 +1,40 @@
 package br.insper.filmes.filme;
 
-public class FilmeService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FilmeService{
+
+    @Autowired
+    private  FilmeRepository filmeRepository;
+
+    public Filme EditarFilme(Filme filme, String id){
+        Optional<Filme> op = filmeRepository.findById(id);
+        if (op.isEmpty()) {
+            throw new IllegalArgumentException("Filme não encontrado");
+        }
+        Filme filmeEditado = op.get();
+        filmeEditado.setTitulo(filme.getTitulo());
+        filmeEditado.setDescricao(filme.getDescricao());
+        filmeEditado.setGenero(filme.getGenero());
+        filmeEditado.setAno(filme.getAno());
+        filmeEditado.setClassificacao(filme.getClassificacao());
+        filmeEditado.setDiretores(filme.getDiretores());
+        filmeEditado.setAtores(filme.getAtores());
+        return filmeRepository.save(filmeEditado);
+    }
+
+    public Filme DeletarFilme(String id){
+        Optional<Filme> op = filmeRepository.findById(id);
+        if (op.isEmpty()) {
+            throw new RuntimeException("Filme não encontrado");
+        }
+        Filme filmeDeletado = op.get();
+        filmeRepository.deleteById(id);
+        return filmeDeletado;
+    }
+
 }
