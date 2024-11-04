a0e372fa99fa617510e3b1e1c62a7ff4fac9a8f2
Ana Helena <anahc3@al.insper.edu.br>
2024-11-04 10:47:54 -0300

    avaliação add

--- filmes/src/main/java/br/insper/filmes/avaliacao/Avaliacao.java
+++ filmes/src/main/java/br/insper/filmes/avaliacao/Avaliacao.java
@@ -1,4 +1,63 @@
 package br.insper.filmes.avaliacao;
 
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
 public class Avaliacao {
+        @Id
+        String id;
+        String comentario;
+        Integer nota;
+        String idFilme;
+        String email;
+        LocalDateTime data;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public String getIdFilme() {
+        return idFilme;
+    }
+
+    public void setIdFilme(String idFilme) {
+        this.idFilme = idFilme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 }
